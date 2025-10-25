52729c6f90612a161ffae3593a2f04c74604f4ce
suyeon <a01037388078@gmail.com>
2025-10-25 17:47:48 +0900

    cleanup: remove image picker and reset project base layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_shopping/pages/add_product_page.dart';
 import 'home_page.dart';
 
 void main() {
@@ -10,6 +11,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
+    return MaterialApp(
+      routes: {
+        "/": (context) => HomePage(), // 홈페이지 라우트
+        "/add-product": (context) => AddProductPage(), // 상품 등록 페이지 라우트
+      },
+      initialRoute: "/", // 앱 시작 시 첫 화면 = 홈페이지
+    );
   }
 }
